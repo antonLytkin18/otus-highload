04f2e5fac90ec1615e1c62f4d01dc121ff432813
Anton Lytkin <a.lytkin@worksolutions.ru>
2020-06-03 00:26:49 +0300

    Forms corrections.

--- monolith/app/auth/forms.py
+++ monolith/app/auth/forms.py
@@ -1,16 +1,27 @@
 from wtforms import IntegerField, StringField, PasswordField, Form, validators
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import EqualTo
+
 
 class RegistrationForm(Form):
     name = StringField(validators=[validators.required()])
-    lastName = StringField(validators=[validators.required()])
+    last_name = StringField(validators=[validators.required()])
     email = StringField(validators=[validators.email()])
-    password = PasswordField('Password', validators=[validators.required(), EqualTo('confirmPassword')])
-    confirmPassword = PasswordField('Confirm Password')
+    password = PasswordField('Password', validators=[validators.required(), EqualTo('confirm_password')])
+    confirm_password = PasswordField('Confirm Password')
     age = IntegerField(validators=[validators.required()])
+    interests = StringField()
     city = StringField(validators=[validators.required()])
 
 
 class AuthForm(Form):
-    email = StringField(validators=[validators.email()])
-    password = PasswordField('Password', validators=[validators.required()])
+    email = EmailField(
+        'Enter e-mail',
+        validators=[validators.email()],
+        render_kw={'placeholder': 'E-mail'}
+    )
+    password = PasswordField(
+        'Enter password',
+        validators=[validators.required()],
+        render_kw={'placeholder': 'Password'}
+    )
