b3263b3b832e7b3d51d14fcbd79a6e12010e186b
Anton Lytkin <a.lytkin@worksolutions.ru>
2020-06-07 23:11:33 +0300

    Redirect from register page when user is authorized.

--- monolith/app/auth/views.py
+++ monolith/app/auth/views.py
@@ -43,6 +43,8 @@ def logout():
 
 @auth.route('/register', methods=['GET'])
 def register_form():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
     return render_template('register.html', form=RegistrationForm())
 
 
