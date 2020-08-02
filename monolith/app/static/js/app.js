8dc54717b57a7c655e860a71c71acc2deed74d13
Anton Lytkin <a.lytkin@worksolutions.ru>
2020-08-02 20:22:36 +0300

    Added Chat Vue components.

--- monolith/app/static/js/app.js
+++ monolith/app/static/js/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import urlMixin from "./mixins/urlMixin.js";
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
 
 Vue.mixin(urlMixin);
 Vue.component('v-form', require('./components/Form.vue').default);
@@ -8,9 +10,15 @@ Vue.component('v-filter-form', require('./components/FilterForm.vue').default);
 Vue.component('v-follower', require('./components/Follower.vue').default);
 Vue.component('v-follower-list', require('./components/FollowerList.vue').default);
 Vue.component('v-follower-actions', require('./components/FollowerActions.vue').default);
+Vue.component('v-chat-message-list', require('./components/Chat/ChatMessageList.vue').default);
+Vue.component('v-chat-list', require('./components/Chat/ChatList.vue').default);
+Vue.component('v-chat-income-message', require('./components/Chat/ChatIncomeMessage.vue').default);
+Vue.component('v-chat-outcome-message', require('./components/Chat/ChatOutcomeMessage.vue').default);
+Vue.component('v-select', vSelect);
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.config.devtools = true
 
 window.axios = require('axios');
 
---
+++ monolith/app/static/js/components/Chat/ChatIncomeMessage.vue
@@ -0,0 +1,47 @@
+<template>
+    <b-row>
+        <b-col cols="9">
+            <b-card style="margin-bottom: 5px;"
+                    no-body
+            >
+                <b-card-body
+                        class="text-muted"
+                        style="background-color: #EFFCDE"
+                >
+                    {{ item['message'] }}
+                </b-card-body>
+                <template v-slot:footer>
+                    <div class="text-left">
+                        <b-avatar
+                                button
+                                variant="secondary"
+                                :size="20"
+                                class="align-center"
+                                @click="onProfileClick($event, item['user_id'])"
+                        ></b-avatar>
+                        <span class="small text-muted">{{ item['user_name'] }}, {{ item['date_create'] }}</span>
+                    </div>
+                </template>
+            </b-card>
+        </b-col>
+        <b-col cols="3"></b-col>
+    </b-row>
+</template>
+
+<script>
+    export default {
+        props: {
+            item: Object | Array,
+            followerUrl: String,
+        },
+        methods: {
+            onProfileClick: function (e, id) {
+                window.open(this.populateUrl(this.followerUrl, {id: id}), '_blank');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
