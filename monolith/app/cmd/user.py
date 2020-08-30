5dda4d76896cfe7293f754245f0c7d6e3c6f4647
Anton Lytkin <a.lytkin@worksolutions.ru>
2020-08-30 23:00:33 +0300

    Added update age and gender commands.

--- monolith/app/cmd/user.py
+++ monolith/app/cmd/user.py
@@ -1,6 +1,9 @@
 import click
+import random
 from app.auth.services import RegistrationService
 from app.db.models import User
+from app.db.repositories import UserRepository
+from datetime import date
 from faker import Faker
 from flask import Blueprint, current_app
 
@@ -22,3 +25,26 @@ def generate(count: int):
             user.password = fake.password()
             user.city = fake.city()
             service.register(user)
+
+
+@user_cmd.cli.command('update-gender')
+def update_gender():
+    gender_male = 1
+    gender_female = 2
+    repository: UserRepository = current_app.injector.get(UserRepository)
+    with click.progressbar(repository.find_all()) as bar:
+        for user in bar:
+            user.gender = random.randint(gender_male, gender_female)
+            repository.save(user)
+
+
+@user_cmd.cli.command('update-age')
+def update_age():
+    repository: UserRepository = current_app.injector.get(UserRepository)
+    today = date.today()
+    with click.progressbar(repository.find_all()) as bar:
+        for user in bar:
+            incomplete_year = (today.month, today.day) < (user.birth_date.month, user.birth_date.day)
+            age = today.year - user.birth_date.year - incomplete_year
+            user.age = age
+            repository.save(user)
