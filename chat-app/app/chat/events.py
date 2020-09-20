e695bffccbaafceb3189a232b4f76111f7737f9e
Anton Lytkin <a.lytkin@worksolutions.ru>
2020-09-20 16:38:02 +0300

    Added read / add message events. Saga pattern.

--- chat-app/app/chat/events.py
+++ chat-app/app/chat/events.py
@@ -1,12 +1,16 @@
 import json
+from datetime import datetime
 
 from blinker import signal
 from flask import current_app
 
-from app.db.models import User
+from app.chat.gateways import CounterAppGateway
+from app.db.models import User, ChatMessage, ChatMessageStatus
 from app.db.repositories import UserRepository
 
 user_registered = signal('user-registered')
+chat_message_added = signal('chat-message-added')
+chat_message_read = signal('chat-message-read')
 
 
 @user_registered.connect
@@ -18,3 +22,35 @@ def register_user(ch, **kwargs):
         name=payload['name'],
         last_name=payload['last_name']
     ))
+
+
+@chat_message_added.connect
+def set_not_read_message_status(chat_service, **kwargs):
+    counter_gateway: CounterAppGateway = kwargs.get('counter_gateway')
+    chat_message: ChatMessage = kwargs.get('chat_message')
+    chat_message_status_repository = kwargs.get('chat_message_status_repository')
+
+    for user_id in chat_service.find_participants(chat_message):
+        chat_message_status = chat_message_status_repository.model_class(
+            user_id=user_id,
+            chat_message_id=chat_message.id,
+            date_create=datetime.now()
+        )
+        chat_message_status.set_not_read_status()
+        chat_message_status_repository.save(chat_message_status)
+
+        if not counter_gateway.incr(chat_message.chat_id, user_id):
+            chat_message_status.set_read_status()
+            chat_message_status_repository.save(chat_message_status)
+
+
+@chat_message_read.connect
+def set_read_message_status(chat_service, **kwargs):
+    counter_gateway: CounterAppGateway = kwargs.get('counter_gateway')
+    chat_message: ChatMessage = kwargs.get('chat_message')
+    chat_message_status: ChatMessageStatus = kwargs.get('chat_message_status')
+    chat_message_status_repository = kwargs.get('chat_message_status_repository')
+
+    if not counter_gateway.decr(chat_message.chat_id, chat_message_status.user_id):
+        chat_message_status.set_not_read_status()
+        chat_message_status_repository.save(chat_message_status)
--- chat-app/app/chat/services.py
+++ chat-app/app/chat/services.py
@@ -1,18 +1,22 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+from blinker import signal
 from injector import inject
 
-from app.db.models import ChatMessage, User, Chat
-from app.db.repositories import ChatRepository, ChatMessageRepository, UserRepository
+from app.chat.gateways import CounterAppGateway
+from app.db.models import ChatMessage, User, Chat, ChatMessageStatus
+from app.db.repositories import ChatRepository, ChatMessageRepository, UserRepository, ChatMessageStatusRepository
 
 
 @dataclass()
 class ChatMessageDTO:
+    id: int = None
     is_current_user: bool = False
     user_id: int = None
     user_name: str = None
     message: str = None
+    is_read: bool = None
     date_create: str = None
 
 
@@ -22,16 +26,20 @@ class ChatDTO:
     user_id: int = None
     user_name: str = None
     last_message: ChatMessageDTO = None
+    unread_messages_count: int = None
 
 
 class ChatService:
 
     @inject
     def __init__(self, user_repository: UserRepository, chat_repository: ChatRepository,
-                 chat_message_repository: ChatMessageRepository):
+                 chat_message_repository: ChatMessageRepository,
+                 chat_message_status_repository: ChatMessageStatusRepository, counter_gateway: CounterAppGateway):
         self.user_repository = user_repository
         self.chat_repository = chat_repository
         self.chat_message_repository = chat_message_repository
+        self.chat_message_status_repository = chat_message_status_repository
+        self.counter_gateway = counter_gateway
 
     def find_or_create_chat(self, from_user_id, to_user_id) -> Chat:
         chat = self.chat_repository.find_one(from_user_id=from_user_id, to_user_id=to_user_id)
@@ -49,6 +57,7 @@ class ChatService:
     def find_chats(self, user_id) -> list:
         result = []
         chats = self.chat_repository.find_all(from_user_id=user_id) + self.chat_repository.find_all(to_user_id=user_id)
+        count_items = self.counter_gateway.count_all(user_id)
         for chat in chats:
             user_to_id = chat.from_user_id if chat.from_user_id != user_id else chat.to_user_id
             user_to: User = self.user_repository.find_one(id=user_to_id)
@@ -59,8 +68,9 @@ class ChatService:
                 user_name=f'{user_to.last_name} {user_to.name}' if user_to else '',
                 last_message=ChatMessageDTO(
                     message=last_message.message if last_message else '',
-                    date_create=last_message.date_create.strftime('%d.%m.%Y, %H:%M') if last_message else ''
+                    date_create=last_message.date_create.strftime('%d.%m.%Y, %H:%M') if last_message else '',
                 ) if last_message else None,
+                unread_messages_count=count_items.get(str(chat.id), None),
             ))
 
         return result
@@ -72,7 +82,15 @@ class ChatService:
             message=message,
             date_create=datetime.now()
         )
-        self.chat_message_repository.save(message)
+        if self.chat_message_repository.save(message):
+            chat_message_added = signal('chat-message-added')
+            chat_message_added.send(
+                self,
+                chat_message=message,
+                chat_message_status_repository=self.chat_message_status_repository,
+                counter_gateway=self.counter_gateway
+            )
+
         user = self.user_repository.find_one(id=message.user_id)
         return ChatMessageDTO(
             is_current_user=True,
@@ -81,6 +99,24 @@ class ChatService:
             message=message.message,
             date_create=message.date_create.strftime('%d.%m.%Y, %H:%M')
         )
+
+    def read_message(self, message_id, user_id):
+        chat_message_statuses = self.chat_message_status_repository.find_all(
+            user_id=user_id,
+            chat_message_id=message_id
+        )
+        for chat_message_status in chat_message_statuses:
+            chat_message_status.set_read_status()
+            if not self.chat_message_status_repository.save(chat_message_status):
+                continue
+            chat_message_read = signal('chat-message-read')
+            chat_message_read.send(
+                self,
+                chat_message=self.chat_message_repository.find_one(id=message_id),
+                chat_message_status=chat_message_status,
+                chat_message_status_repository=self.chat_message_status_repository,
+                counter_gateway=self.counter_gateway
+            )
 
     def find_messages(self, chat_id, user_id) -> list:
         result = []
@@ -91,11 +127,22 @@ class ChatService:
             if not user:
                 user = self.user_repository.find_one(id=message.user_id)
                 users_cached[user.id] = user
+            chat_message_status: ChatMessageStatus = self.chat_message_status_repository.find_one(
+                chat_message_id=message.id
+            )
             result.append(ChatMessageDTO(
+                id=message.id,
                 is_current_user=user_id == user.id,
                 user_id=user.id,
                 user_name=f'{user.last_name} {user.name}' if user else '',
                 message=message.message,
+                is_read=chat_message_status.is_read if chat_message_status else True,
                 date_create=message.date_create.strftime('%d.%m.%Y, %H:%M')
             ))
         return result
+
+    def find_participants(self, chat_message: ChatMessage) -> list:
+        chat = self.chat_repository.find_one(id=chat_message.chat_id)
+        if not chat:
+            return []
+        return [user_id for user_id in [chat.from_user_id, chat.to_user_id] if user_id != chat_message.user_id]
