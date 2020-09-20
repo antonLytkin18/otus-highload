b2cbb7eac8ddb6c44b068dd4ae31416e9f1a6009
Anton Lytkin <a.lytkin@worksolutions.ru>
2020-09-20 16:36:28 +0300

    Added dependencies.

--- chat-app/app/dependencies.py
+++ chat-app/app/dependencies.py
@@ -2,12 +2,16 @@ from injector import singleton
 
 from app.chat.services import ChatService
 
+from app.chat.gateways import CounterAppGateway
 from app.db.db import DbConnectionPool, Db
-from app.db.repositories import ChatRepository, UserRepository
+from app.db.repositories import ChatRepository, ChatMessageRepository, ChatMessageStatusRepository, UserRepository
 
 
 def configure(binder):
     binder.bind(DbConnectionPool, to=Db, scope=singleton)
     binder.bind(UserRepository, scope=singleton)
     binder.bind(ChatRepository, scope=singleton)
+    binder.bind(ChatMessageRepository, scope=singleton)
+    binder.bind(ChatMessageStatusRepository, scope=singleton)
     binder.bind(ChatService, scope=singleton)
+    binder.bind(CounterAppGateway, scope=singleton)
